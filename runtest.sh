3c378c11252bb728c6acf9f1a1ac02557ebb13c4
Paul Lee <chaiyapong@3dsinteractive.com>
2017-08-01 17:34:56 +0700

    Update docker id.

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -it pamsdkphp_php56_1 composer test tests
-docker exec -it pamsdkphp_php70_1 composer test tests
+docker exec -it pamsdkphp_php56_1 composer test tests/SdkTest.php
+docker exec -it pamsdkphp_php70_1 composer test tests/SdkTest.php
